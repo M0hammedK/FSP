73712439a4fe287a95133426954cb2939d906d2e
M0hammedK <96379480+M0hammedK@users.noreply.github.com>
2024-11-27 09:39:55 +0300

    sing_email in paymentpage.dart

--- lib/paymentpage.dart
+++ lib/paymentpage.dart
@@ -191,7 +191,7 @@ class _paymentpageState extends State<paymentpage> {
                             });
                             MovieData().Tickets.add({
                               'title': widget.title, // Movie title
-                              'userEmail': sign_up.email.text,
+                              'userEmail': sing_in.email.text,
                               'moviePost': widget.posterUrl,
                               'duration': widget.duration,
                               'date': widget.date
